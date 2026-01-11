0acad35a5dcacc75608b8f3f3b4486f7936876ad
AyaHassan55 <haya98166@gmail.com>
2026-01-11 16:48:42 +0200

    header section done

---
+++ app/_component/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+export default function Header() {
+    const Menu = [
+        {
+            id: 1,
+            name: "Home",
+            path: "/"
+        },
+        {
+            id: 2,
+            name: "Explore",
+            path: "/explore"
+        },
+        {
+            id: 3,
+            name: "Contact US",
+            path: "/contact"
+        }
+    ]
+    return (
+        <div className='flex items-center justify-between p-3 shadow-sm'>
+            <div className='flex items-center gap-10'>
+                <Image src={"/assets/images/logo.png"} alt="logo" width={100} height={100} />
+                <ul className='md:flex items-center gap-8 hidden'>
+                    {Menu.map((item, index) => (
+                        <Link href={item.path}>
+                        <li className='hover:text-lime-600 cursor-pointer hover:scale-105 transition-all'>{item.name}</li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <Button>Get Started</Button>
+        </div>
+    )
+}
+
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
+    <div className="grid grid-rows-[20px_1fr_20px] itrms-center justify-items-center h-screen">
+       <Button >Hello World</Button>
     </div>
   );
 }
