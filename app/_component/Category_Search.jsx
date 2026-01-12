1286a0c115e5667573bd02dfce388b8b660e34ed
AyaHassan55 <haya98166@gmail.com>
2026-01-12 20:30:29 +0200

    fetch category is done

--- app/_component/Category_Search.jsx
+++ app/_component/Category_Search.jsx
@@ -26,15 +26,19 @@ export default function CategorySearch() {
                 <Input placeholder="Search for catgory" className='w-full mt-4 mb-8 mx-3' />
                 <Button type='submit' className="mb-4">Submit</Button>
             </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 mt-8'>
+                {categoriesList.map((cat, index) => (
+                    <div key={cat.id} className='flex flex-col items-center text-center m-2 bg-lime-200 rounded-lg p-5 hover:scale-110 transition-all duration-300 cursor-pointer'>
+                        <Image src={`http://localhost:1337${cat?.icon[0]?.url}`}
+                            width={50} height={50} alt={cat.name}
+                            unoptimized
+                        />
+                        <p className="mt-2 font-medium">{cat?.name}</p>
 
-            {categoriesList.map((cat,index)=>(
-                <div  key={cat.id}>
-                    <Image src={`http://localhost:1337${cat?.icon[0]?.url}`} 
-                      width={70} height={70} alt={cat.name}
-                      unoptimized
-                      />
-                </div>
-            ))}
+                    </div>
+
+                ))}
+            </div>
         </div>
     )
 }
